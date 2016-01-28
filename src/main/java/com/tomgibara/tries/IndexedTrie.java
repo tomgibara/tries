ecbe4e315778b243b3b5f296d6408830474c0868
Tom Gibara <me@tomgibara.com>
2016-01-28 06:42:55 +0000

    Removes redundant rootIndex from indexed trie.

--- src/main/java/com/tomgibara/tries/IndexedTrie.java
+++ src/main/java/com/tomgibara/tries/IndexedTrie.java
@@ -8,11 +8,6 @@ import java.util.NoSuchElementException;
 
 public class IndexedTrie<E> extends Trie<E> {
 
-	// fields
-	
-	//TODO is this needed?
-	private int rootIndex = 0;
-
 	// constructors
 	
 	IndexedTrie(Tries<E> tries, TrieNodes nodes) {
@@ -69,17 +64,13 @@ public class IndexedTrie<E> extends Trie<E> {
 	// package scoped methods
 	
 	@Override
-	//TODO pity we can't perform count and find at the same time
 	TrieNode findRoot(byte[] bytes, int length) {
 		TrieNode node = nodes.root();
-		int index = 0;
 		for (int i = 0; i < length; i++) {
 			byte value = bytes[i];
-			index += node.countToChild(value);
 			node = node.findChild(value);
 			if (node == null) break;
 		}
-		rootIndex = index;
 		return node;
 
 	}
